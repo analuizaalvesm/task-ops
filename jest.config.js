69680442948a415974d1841fe3b559bdcbeaf497
analuizaalvesm <analuizaalvesm@gmail.com>
2025-11-28 11:33:10 -0300

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,12 @@ module.exports = {
   testEnvironment: "node",
   roots: ["<rootDir>/src"],
   testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
+  testPathIgnorePatterns: ["/node_modules/", "/e2e/"],
   moduleFileExtensions: ["ts", "js", "json"],
   collectCoverageFrom: [
     "src/**/*.ts",
     "!src/**/*.test.ts",
     "!src/**/*.spec.ts",
+    "!src/tests/e2e/**",
   ],
 };
